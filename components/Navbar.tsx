c3ca5b53993ef2d55dea2fc9c55cbc8cdc70fe6a
mixer1995 <mixer19995@gmail.com>
2024-02-27 14:25:13 +0200

    Added mobile versions for header and main content

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,28 @@
 import styles from './Navbar.module.scss'
 import ratepunkLogo from './../public/logo.svg'
+import hamburgLogo from './../public/menu.svg'
+import closeLogo from './../public/close.svg'
+import { useState } from 'react';
 
 const Navbar = () => {
-  console.log(ratepunkLogo);
+  const [opened, setOpened] = useState(false);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${opened && styles.opened}`}>
       <nav>
-        <img src={ratepunkLogo.src} alt="ratepunkLogo" />
-        <ul>
+        <img src={ratepunkLogo.src} alt="ratepunkLogo" className={styles.rateLogo}/>
+        <ul className={styles.desctopLi}>
           <li><a href="#">Chrome Extension</a></li>
           <li><a href="#">Price Comparison</a></li>
           <li><a href="#">Blog</a></li>
         </ul>
+        <img src={opened ? closeLogo.src : hamburgLogo.src} alt="hamburgCloseLogo" className={styles.hamburgCloseLogo} onClick={() => setOpened(!opened)} />
       </nav>
+      <ul className={`${styles.mobileLi} ${opened && styles.openedLi}`}>
+          <li><a href="#">Chrome Extension</a></li>
+          <li><a href="#">Price Comparison</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
     </header>
   )
 }
